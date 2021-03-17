3a0a92fc79133335490764ddbd1f1f975d23b257
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2021-03-17 15:32:31 +0000

    Don't store filenames as tags/names.

--- src/modules/neutronsq/process.cpp
+++ src/modules/neutronsq/process.cpp
@@ -173,7 +173,7 @@ bool NeutronSQModule::process(Dissolve &dissolve, ProcessPool &procPool)
     auto &weightedSQ = dissolve.processingModuleData().realise<PartialSet>("WeightedSQ", uniqueName_,
                                                                            GenericItem::InRestartFileFlag, &created);
     if (created)
-        weightedSQ.setUpPartials(unweightedSQ.atomTypes(), uniqueName(), "weighted", "sq", "Q, 1/Angstroms");
+        weightedSQ.setUpPartials(unweightedSQ.atomTypes(), uniqueName_, "weighted", "sq", "Q, 1/Angstroms");
 
     // Calculate weighted S(Q)
     calculateWeightedSQ(unweightedSQ, weightedSQ, weights, normalisation);
@@ -182,7 +182,7 @@ bool NeutronSQModule::process(Dissolve &dissolve, ProcessPool &procPool)
     weightedSQ.setObjectTags(fmt::format("{}//{}", uniqueName_, "WeightedSQ"));
 
     // Save data if requested
-    if (saveSQ && (!MPIRunMaster(procPool, weightedSQ.save())))
+    if (saveSQ && (!MPIRunMaster(procPool, weightedSQ.save(uniqueName_, "weighted", "sq"))))
         return false;
 
     /*
@@ -207,7 +207,7 @@ bool NeutronSQModule::process(Dissolve &dissolve, ProcessPool &procPool)
     weightedGR.setObjectTags(fmt::format("{}//{}", uniqueName_, "WeightedGR"));
 
     // Save data if requested
-    if (saveGR && (!MPIRunMaster(procPool, weightedGR.save())))
+    if (saveGR && (!MPIRunMaster(procPool, weightedGR.save(uniqueName_, "weighted", "gr"))))
         return false;
 
     // Calculate representative total g(r) from FT of calculated S(Q)
